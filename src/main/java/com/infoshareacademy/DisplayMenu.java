a0d30393b0f20c43036bbb6ca696c9da241ea9e7
jakubcichacki <kuba.cichacki@o2.pl>
2020-04-20 21:58:47 +0200

    Add logger in showing menu method

--- src/main/java/com/infoshareacademy/DisplayMenu.java
+++ src/main/java/com/infoshareacademy/DisplayMenu.java
@@ -1,15 +1,19 @@
 package com.infoshareacademy;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import java.util.Scanner;
 
 public class DisplayMenu <T extends Enum> {
+    private final static Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     Scanner scanner = new Scanner(System.in);
 
     public T showMenu(T[] enums) {
         Integer choice;
         do {
             for (T m : enums) {
-                System.out.println(m.ordinal() + " - " + m);
+                STDOUT.info(m.ordinal() + " - " + m + "\n");
             }
             choice = getIntFromKeyboard();
         } while (choice == null);
