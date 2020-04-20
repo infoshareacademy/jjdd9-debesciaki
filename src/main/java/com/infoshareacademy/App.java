ce86e81084b98c0abf45d881e86bc3b3c784ace7
jakubcichacki <kuba.cichacki@o2.pl>
2020-04-20 21:52:57 +0200

    Apply logger to files

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,6 +1,10 @@
 package com.infoshareacademy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private final static Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
     public static void main(String[] args) {
         MenuController main = new MenuController();
         main.run();
