aed18e33193ee19ec6c9c87a465b0e64305a906f
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-04-23 16:06:22 +0200

    Do some progress

--- src/main/java/com/infoshareacademy/display/Display.java
+++ src/main/java/com/infoshareacademy/display/Display.java
@@ -1,17 +1,15 @@
 package com.infoshareacademy.display;
 
 import com.infoshareacademy.parser.Event;
-import com.infoshareacademy.parser.Place;
 import com.infoshareacademy.repository.EventRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
 
 public class Display {
     private final static Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
@@ -21,20 +19,20 @@ public class Display {
     }
 
     public void currentEvents() throws IOException, InterruptedException {
-        Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        DateToInt actualT = new DateToInt();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+        STDOUT.info("{}\n", LocalDate.now().format(formatter));
+        DateTimeFormatter eventFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
 
-        actualT.convert(formatter.format(date));
-
-        //Ask for how many elements user wants to see
-        Scanner scanner = new Scanner(System.in);
-        STDOUT.info("Ile nadchodzących wydarzeń chcesz zobaczyć? --> ");
-        int qty = scanner.nextInt();
-
-        Map<Integer, Event> eventMap = selectedMap(actualT, qty);
+        //sprawdź czy null
+        Optional<Integer> optInt = inputElementQuantity();
+        Integer qty = optInt.get();
+        STDOUT.info("Number is: {} \n", qty);
 
 
+
+        //Map<Integer, Event> eventMap = selectedMap(actualT, qty);
+
+        /*
         for (Event e : eventMap.values()) {
             Place p = e.getPlace();
             STDOUT.info("Name: {}{}{}\nPlace: {}{}{} \nEnd date: {}{}{}\n", ConsoleColor.RED_UNDERLINED, e.getName(), ConsoleColor.RESET,
@@ -42,6 +40,23 @@ public class Display {
                     ConsoleColor.CYAN_BACKGROUND, e.getEndDate(), ConsoleColor.RESET);
         }
         STDOUT.info("size of map {}\nsize of a repository  {}\n", eventMap.size(), EventRepository.getAllEvents().size());
+
+         */
+    }
+
+    public Optional<Integer> inputElementQuantity() {
+        Integer qty = null;
+        Optional<Integer> opt = Optional.ofNullable(qty);
+        do {
+            STDOUT.info("Ile nadchodzących wydarzeń chcesz zobaczyć? --> ");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                opt = Optional.ofNullable(qty = scanner.nextInt());
+            } catch (InputMismatchException e) {
+                STDOUT.info("Źle wprowadzone dane, spróbuj ponownie! msg: {}\n", e.getMessage());
+            }
+        } while (opt.isEmpty());
+        return opt;
     }
 
     public Map<Integer, Event> selectedMap(DateToInt actualT, int qty) {
@@ -70,5 +85,4 @@ public class Display {
         }
         return eventMap;
     }
-    public DateToInt actualTime(){return null;}
 }
