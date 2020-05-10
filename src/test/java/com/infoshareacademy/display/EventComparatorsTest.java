658c05a61faf901cd2cd3def216c96b6cdaacc71
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-05-10 11:12:40 +0200

    Add comparator tests

---
+++ src/test/java/com/infoshareacademy/display/EventComparatorsTest.java
@@ -0,0 +1,198 @@
+package com.infoshareacademy.display;
+
+import com.infoshareacademy.parser.Event;
+import com.infoshareacademy.parser.Organizer;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+class EventComparatorsTest {
+    @Test
+    void byNameAsc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        e1.setName("Name");
+        Event e2 = new Event();
+        e2.setName("Mame");
+        int result;
+
+        //WHEN
+        result = EventComparators.EventNameComparatorAsc.compare(e1, e2);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    void byNameAsc_expect0() {
+        //GIVEN
+        Event e1 = new Event();
+        e1.setName("Name");
+        Event e2 = new Event();
+        e2.setName("Name");
+        int result;
+
+        //WHEN
+        result = EventComparators.EventNameComparatorAsc.compare(e1, e2);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(0);
+
+    }
+
+    @Test
+    void byNameAsc_expect_1() {
+        //GIVEN
+        Event e1 = new Event();
+        e1.setName("Name");
+        Event e2 = new Event();
+        e2.setName("Mame");
+        int result;
+
+        //WHEN
+        result = EventComparators.EventNameComparatorAsc.compare(e2, e1);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(-1);
+
+    }
+
+    @Test
+    void byNameDesc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        e1.setName("Name");
+        Event e2 = new Event();
+        e2.setName("Mame");
+        int result;
+
+        //WHEN
+        result = EventComparators.EventNameComparatorDesc.compare(e2, e1);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    void byDateAsc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        LocalDateTime localDateTimeNow = LocalDateTime.now();
+        LocalDateTime localDateTimeBefore = localDateTimeNow.minusDays(10);
+        e1.setEndDate(localDateTimeNow);
+        e2.setEndDate(localDateTimeBefore);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventEndDateComparatorAsc.compare(e1, e2);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    void byDateDesc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        LocalDateTime localDateTimeNow = LocalDateTime.now();
+        LocalDateTime localDateTimeBefore = localDateTimeNow.minusDays(10);
+        e1.setEndDate(localDateTimeNow);
+        e2.setEndDate(localDateTimeBefore);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventEndDateComparatorDesc.compare(e2, e1);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    void byOrganizerAsc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        Organizer o1 = new Organizer();
+        Organizer o2 = new Organizer();
+        o1.setDesignation("A");
+        o2.setDesignation("B");
+        e1.setOrganizer(o1);
+        e2.setOrganizer(o2);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventOrganizerComparatorAsc.compare(e2,e1);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+
+    @Test
+    void byOrganizerDesc_expect1() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        Organizer o1 = new Organizer();
+        Organizer o2 = new Organizer();
+        o1.setDesignation("A");
+        o2.setDesignation("B");
+        e1.setOrganizer(o1);
+        e2.setOrganizer(o2);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventOrganizerComparatorDesc.compare(e1,e2);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(1);
+
+    }
+    @Test
+    void byOrganizerDesc_expect0() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        Organizer o1 = new Organizer();
+        Organizer o2 = new Organizer();
+        o1.setDesignation("A");
+        o2.setDesignation("A");
+        e1.setOrganizer(o1);
+        e2.setOrganizer(o2);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventOrganizerComparatorDesc.compare(e1,e2);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(0);
+
+    }
+
+    @Test
+    void byOrganizerDesc_expect_1() {
+        //GIVEN
+        Event e1 = new Event();
+        Event e2 = new Event();
+        Organizer o1 = new Organizer();
+        Organizer o2 = new Organizer();
+        o1.setDesignation("A");
+        o2.setDesignation("B");
+        e1.setOrganizer(o1);
+        e2.setOrganizer(o2);
+        int result;
+
+        //WHEN
+        result = EventComparators.EventOrganizerComparatorDesc.compare(e2,e1);
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(-1);
+
+    }
+}
--- src/test/java/com/infoshareacademy/parser/EventTest.java
+++ src/test/java/com/infoshareacademy/parser/EventTest.java
@@ -3,8 +3,8 @@ package com.infoshareacademy.parser;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class EventTest {
 
@@ -62,7 +62,7 @@ class EventTest {
         LocalDateTime result;
 
         //WHEN
-        result=e.getEndDate();
+        result = e.getEndDate();
 
         //THEN
         org.assertj.core.api.Assertions.assertThat(result).isEqualTo(localDateTime);
@@ -78,10 +78,155 @@ class EventTest {
         LocalDateTime result;
 
         //WHEN
-        result=e.getStartDate();
+        result = e.getStartDate();
 
         //THEN
         org.assertj.core.api.Assertions.assertThat(result).isEqualTo(localDateTime);
+
+    }
+
+    @Test
+    void getSetUrls() {
+        //GIVEN
+        Event e = new Event();
+        Url u = new Url();
+        u.setFb("FB");
+        u.setTickets("TICKETS");
+        u.setWww("WWW");
+        Url result;
+        e.setUrls(u);
+
+        //WHEN
+        result = e.getUrls();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(u);
+        org.assertj.core.api.Assertions.assertThat(result.getWww()).isEqualTo(u.getWww());
+        org.assertj.core.api.Assertions.assertThat(result.getTickets()).isEqualTo(u.getTickets());
+        org.assertj.core.api.Assertions.assertThat(result.getFb()).isEqualTo(u.getFb());
+
+    }
+
+    @Test
+    void getSetCategoryId() {
+        //GIVEN
+        Event e = new Event();
+        e.setCategoryId(55);
+        int result;
+
+        //WHEN
+        result = e.getCategoryId();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(55);
+
+    }
+
+    @Test
+    void getSetAttachments() {
+        //GIVEN
+        Event e = new Event();
+        List<Attachment> attachments = new ArrayList<>();
+        List<Attachment> result = new ArrayList<>();
+        Attachment a1 = new Attachment();
+        Attachment a2 = new Attachment();
+        Attachment a3 = new Attachment();
+        a1.setFileName("name1");
+        a2.setFileName("name2");
+        a3.setFileName("name3");
+        attachments.add(a1);
+        attachments.add(a2);
+        attachments.add(a3);
+        e.setAttachments(attachments);
+
+        //WHEN
+        result = e.getAttachments();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(attachments);
+
+    }
+
+    @Test
+    void getSetDescShort() {
+        //GIVEN
+        Event e = new Event();
+        e.setDescShort("SHORT");
+        String result;
+
+        //WHEN
+        result = e.getDescShort();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo("SHORT");
+
+    }
+
+    @Test
+    void getSetDescLong() {
+        //GIVEN
+        Event e = new Event();
+        e.setDescLong("LONG");
+        String result;
+
+        //WHEN
+        result = e.getDescLong();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo("LONG");
+
+    }
+
+    @Test
+    void getSetTickets() {
+        //GIVEN
+        Event e = new Event();
+        Ticket t = new Ticket();
+        t.setType("typ");
+        e.setTickets(t);
+        Ticket result;
+
+        //WHEN
+        result = e.getTickets();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(t);
+
+    }
+
+    @Test
+    void getSetOrganizer() {
+        //GIVEN
+        Event e = new Event();
+        Organizer o = new Organizer();
+        o.setDesignation("Things");
+        o.setId(11);
+        e.setOrganizer(o);
+        Organizer result;
+
+        //WHEN
+        result = e.getOrganizer();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(o);
+
+    }
+
+    @Test
+    void getSetPlace() {
+        //GIVEN
+        Event e = new Event();
+        Place p = new Place();
+        Address a = new Address();
+        p.setAddress(a);
+        e.setPlace(p);
+        Place result;
+
+        //WHEN
+        result = e.getPlace();
+
+        //THEN
+        org.assertj.core.api.Assertions.assertThat(result).isEqualTo(p);
 
     }
 }
