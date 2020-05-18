ed63ad161924907d720d90d4238f3ebba3fb7403
jakubcichacki <kuba.cichacki@o2.pl>
2020-05-18 12:08:21 +0200

    Prepare servlet for main page plus change field name in logger check

---
+++ web/src/main/java/com/infoshareacademy/servlet/MainPageServlet.java
@@ -0,0 +1,43 @@
+package com.infoshareacademy.servlet;
+
+import com.infoshareacademy.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/index")
+public class MainPageServlet extends HttpServlet {
+    private static final Logger STDLOG = LoggerFactory.getLogger(LoggerCheck.class.getName());
+
+    @Inject
+    TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Template template = templateProvider.getTemplate(getServletContext(), "index.ftlh");
+        Map<String, Object> dataModel = new HashMap<>();
+
+        PrintWriter pw = resp.getWriter();
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        try {
+            template.process(dataModel, pw);
+        } catch (TemplateException e) {
+            STDLOG.error("Template for main page error");
+        }
+
+    }
+}
