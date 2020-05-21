4e2f81c22edd591d38f041c94ce65db60a87533d
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-05-21 15:54:00 +0200

    Update DB related files

--- sqlNoConstraint.sql
+++ sqlNoConstraint.sql
@@ -51,43 +51,10 @@ CREATE TABLE IF NOT EXISTS `gwk`.`category`
     `root_category_id` BIGINT NULL,
     PRIMARY KEY (`id`),
     INDEX `root_category_id_idx` (`root_category_id` ASC),
-    CONSTRAINT `root_category_id`
-        FOREIGN KEY (`root_category_id`)
-            REFERENCES `gwk`.`category` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8
-    COLLATE = utf8_polish_ci;
-
-
--- -----------------------------------------------------
--- Table `gwk`.`ticket`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `gwk`.`ticket`
-(
-    `id`           BIGINT      NOT NULL,
-    `type`         VARCHAR(45) NULL,
-    `start_ticket` INT         NULL,
-    `end_ticket`   INT         NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8
-    COLLATE = utf8_polish_ci;
-
-
--- -----------------------------------------------------
--- Table `gwk`.`urls`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `gwk`.`urls`
-(
-    `id`      BIGINT      NOT NULL,
-    `www`     VARCHAR(50) NULL,
-    `tickets` VARCHAR(45) NULL,
-    `fb`      VARCHAR(45) NULL,
-    PRIMARY KEY (`id`)
+    FOREIGN KEY (`root_category_id`)
+        REFERENCES `gwk`.`category` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8
@@ -99,15 +66,13 @@ CREATE TABLE IF NOT EXISTS `gwk`.`urls`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `gwk`.`event`
 (
-    `id`           BIGINT NOT NULL,
+    `id`           BIGINT NOT NULL AUTO_INCREMENT,
     `name`         TEXT   NULL,
     `end_date`     DATE   NULL,
     `start_date`   DATE   NULL,
     `active`       INT    NULL,
     `desc_long`    TEXT   NULL,
     `desc_short`   TEXT   NULL,
-    `ticket_id`    BIGINT NULL,
-    `urls_id`      BIGINT NULL,
     `attachments`  TEXT   NULL,
     `place_id`     BIGINT NULL,
     `organizer_id` BIGINT NULL,
@@ -116,33 +81,18 @@ CREATE TABLE IF NOT EXISTS `gwk`.`event`
     INDEX `place_id_idx` (`place_id` ASC),
     INDEX `organizer_id_idx` (`organizer_id` ASC),
     INDEX `category_id_idx` (`category_id` ASC),
-    INDEX `ticket_id_idx` (`ticket_id` ASC),
-    INDEX `urls_id_idx` (`urls_id` ASC),
-    CONSTRAINT `place_id`
-        FOREIGN KEY (`place_id`)
-            REFERENCES `gwk`.`place` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `organizer_id`
-        FOREIGN KEY (`organizer_id`)
-            REFERENCES `gwk`.`organizer` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `category_id`
-        FOREIGN KEY (`category_id`)
-            REFERENCES `gwk`.`category` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `ticket_id`
-        FOREIGN KEY (`ticket_id`)
-            REFERENCES `gwk`.`ticket` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `urls_id`
-        FOREIGN KEY (`urls_id`)
-            REFERENCES `gwk`.`urls` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    FOREIGN KEY (`place_id`)
+        REFERENCES `gwk`.`place` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (`organizer_id`)
+        REFERENCES `gwk`.`organizer` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (`category_id`)
+        REFERENCES `gwk`.`category` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8
@@ -154,15 +104,15 @@ CREATE TABLE IF NOT EXISTS `gwk`.`event`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `gwk`.`address`
 (
-    `street`     TEXT   NULL,
-    `zipcode`    TEXT   NULL,
-    `city`       TEXT   NULL,
-    `lat`        DOUBLE NULL,
-    `lng`        DOUBLE NULL,
-    `place_id`   BIGINT NOT NULL,
-    `placefk_id` BIGINT not null,
-    PRIMARY KEY (`place_id`),
-    FOREIGN KEY (`placefk_id`)
+    `street`   TEXT   NULL,
+    `zipcode`  TEXT   NULL,
+    `city`     TEXT   NULL,
+    `id`       BIGINT NOT NULL AUTO_INCREMENT,
+    `lat`      DOUBLE NULL,
+    `lng`      DOUBLE NULL,
+    `place_id` BIGINT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`place_id`)
         REFERENCES `gwk`.`place` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION
@@ -198,11 +148,10 @@ CREATE TABLE IF NOT EXISTS `gwk`.`user`
     PRIMARY KEY (`id`),
     UNIQUE INDEX `mail_UNIQUE` (`mail` ASC),
     INDEX `role_id_idx` (`role_id` ASC),
-    CONSTRAINT `role_id`
-        FOREIGN KEY (`role_id`)
-            REFERENCES `gwk`.`role` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    FOREIGN KEY (`role_id`)
+        REFERENCES `gwk`.`role` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8
@@ -218,16 +167,78 @@ CREATE TABLE IF NOT EXISTS `gwk`.`favourite`
     `event_id` BIGINT NULL,
     INDEX `user_id_idx` (`user_id` ASC),
     INDEX `event_id_idx` (`event_id` ASC),
-    CONSTRAINT `user_id`
-        FOREIGN KEY (`user_id`)
-            REFERENCES `gwk`.`user` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `event_id`
-        FOREIGN KEY (`event_id`)
-            REFERENCES `gwk`.`event` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+    FOREIGN KEY (`user_id`)
+        REFERENCES `gwk`.`user` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    FOREIGN KEY (`event_id`)
+        REFERENCES `gwk`.`event` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8
+    COLLATE = utf8_polish_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gwk`.`ticket`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gwk`.`ticket`
+(
+    `id`           BIGINT      NOT NULL AUTO_INCREMENT,
+    `type`         VARCHAR(45) NULL,
+    `start_ticket` INT         NULL,
+    `end_ticket`   INT         NULL,
+    `event_id`     BIGINT      NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `event_id_idx` (`event_id` ASC),
+    FOREIGN KEY (`event_id`)
+        REFERENCES `gwk`.`event` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8
+    COLLATE = utf8_polish_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gwk`.`urls`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gwk`.`urls`
+(
+    `id`       BIGINT      NOT NULL AUTO_INCREMENT,
+    `www`      VARCHAR(50) NULL,
+    `tickets`  VARCHAR(45) NULL,
+    `fb`       VARCHAR(45) NULL,
+    `event_id` BIGINT      NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `event_id_idx` (`event_id` ASC),
+    FOREIGN KEY (`event_id`)
+        REFERENCES `gwk`.`event` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8
+    COLLATE = utf8_polish_ci;
+
+
+-- -----------------------------------------------------
+-- Table `gwk`.`attachment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gwk`.`attachment`
+(
+    `id`        BIGINT      NOT NULL AUTO_INCREMENT,
+    `event_id`  BIGINT      NOT NULL,
+    `file_name` VARCHAR(50) NULL,
+    PRIMARY KEY (`id`),
+    INDEX `event_id_idx` (`event_id` ASC),
+    FOREIGN KEY (`event_id`)
+        REFERENCES `gwk`.`event` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8
