789baa149eddadba633b2fae4eb6d6a6cc34e7ff
jakubcichacki <kuba.cichacki@o2.pl>
2020-05-26 16:02:25 +0200

    Add ticket mapper

---
+++ web/src/main/java/com/infoshareacademy/mapper/TicketMapper.java
@@ -0,0 +1,26 @@
+package com.infoshareacademy.mapper;
+
+import com.infoshareacademy.classJSONs.TicketJSON;
+import com.infoshareacademy.entityDAO.Ticket;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class TicketMapper {
+
+    public TicketJSON daoToJson(Ticket ticket) {
+        TicketJSON jsonTicket = new TicketJSON();
+        jsonTicket.setType(ticket.getType());
+        jsonTicket.setStartTicket(ticket.getStartTicket());
+        jsonTicket.setEndTicket(ticket.getEndTicket());
+        return jsonTicket;
+    }
+
+    public Ticket jsonToDao(Ticket ticket) {
+        Ticket daoTicket = new Ticket();
+        daoTicket.setType(ticket.getType());
+        daoTicket.setStartTicket(ticket.getStartTicket());
+        daoTicket.setEndTicket(ticket.getEndTicket());
+        return daoTicket;
+    }
+}
