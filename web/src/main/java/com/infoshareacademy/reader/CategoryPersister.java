193f4a05ab96a5fceb6fee1abe520f445d37a487
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-05-26 18:46:19 +0200

    Add OrganizerPersister bean

--- web/src/main/java/com/infoshareacademy/reader/CategoryPersister.java
+++ web/src/main/java/com/infoshareacademy/reader/CategoryPersister.java
@@ -7,13 +7,14 @@ import com.infoshareacademy.classJSONs.CategoryJSON;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Stateless
 public class CategoryPersister {
     @Inject
     CategoryMapper
-    public void fromJsonFile(File file){
+    public void fromJsonFile(File file) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         List<CategoryJSON> categoryList = mapper.readValue(file, new TypeReference<List<CategoryJSON>>() {
         });
---
+++ web/src/main/java/com/infoshareacademy/reader/OrganizerPersister.java
@@ -0,0 +1,32 @@
+package com.infoshareacademy.reader;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.infoshareacademy.classJSONs.OrganizerJSON;
+import com.infoshareacademy.mapper.OrganizerMapper;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Stateless
+public class OrganizerPersister {
+    @Inject
+    OrganizerMapper organizerMapper;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void fromJsonFile(File file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<OrganizerJSON> organizerJSON = mapper.readValue(file, new TypeReference<List<OrganizerJSON>>() {
+        });
+        for (OrganizerJSON o : organizerJSON) {
+            entityManager.persist(organizerMapper.jsonToDao(o));
+        }
+    }
+}
