e6cd7fb45d80e430be1247a798e64ed6a90c97ec
Bartłomiej Trawiński <bartraw23@gmail.com>
2020-05-26 14:47:42 +0200

    Fix LoginServlet Logger

--- web/src/main/java/com/infoshareacademy/servlet/LoginServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/LoginServlet.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    private static final Logger STDLOG = LoggerFactory.getLogger(LoggerCheck.class.getName());
+    private static final Logger STDLOG = LoggerFactory.getLogger(LoginServlet.class.getName());
 
     @Inject
     TemplateProvider templateProvider;
