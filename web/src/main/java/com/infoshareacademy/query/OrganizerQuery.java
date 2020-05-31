064687e4529007d71c3c1d79232371b3d94c3646
jakubcichacki <kuba.cichacki@o2.pl>
2020-05-31 18:11:35 +0200

    Prepare organizers view

--- web/src/main/java/com/infoshareacademy/query/OrganizerQuery.java
+++ web/src/main/java/com/infoshareacademy/query/OrganizerQuery.java
@@ -6,6 +6,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.List;
 import java.util.Optional;
 
 @Stateless
@@ -17,5 +18,23 @@ public class OrganizerQuery {
         Query query = entityManager.createQuery("SELECT c FROM Organizer c WHERE c.apiId=:apiID");
         query.setParameter("apiID", id);
         return Optional.ofNullable((Organizer) query.getSingleResult());
+    }
+
+    private final static int MAX_RESULTS = 20;
+
+    public List<Organizer> organizersListWithLimit(int firstElement) {
+        Query query = entityManager.createNamedQuery("Organizer.findAll");
+        query.setFirstResult(firstElement).setMaxResults(MAX_RESULTS);
+        return query.getResultList();
+    }
+
+    public List<Organizer> allOrganizersList () {
+        Query query = entityManager.createNamedQuery("Organizer.findAll");
+        return query.getResultList();
+    }
+
+    public Integer sizeList() {
+        Query query = entityManager.createNamedQuery("Organizer.findAll");
+        return query.getResultList().size();
     }
 }
