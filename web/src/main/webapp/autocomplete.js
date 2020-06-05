332ef9ba62e445bbcc37a7ce8088c0ca870e4a3d
Bartek <bartraw23@gmail.com>
2020-06-05 21:33:21 +0200

    functioning autocomplete with separetion of eve and org disparities

--- web/src/main/webapp/autocomplete.js
+++ web/src/main/webapp/autocomplete.js
@@ -21,35 +21,36 @@ function autocomplete(inp, arr, arr2) {
         console.log('arr length ' + arr.length);
 
         for (i = 0; i < arr.length; i++) {
+            q1 = 'show-one-event?event=';
+            arr2.push(arr[i].id);
+
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+
+            /*make the matching letters bold:*/
+
             /*check if the item starts with the same letters as the text field value:*/
             if (arr[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                q1 = 'show-one-event?event=';
-                arr2.push(arr[i].id);
-                /*create a DIV element for each matching element:*/
-                b = document.createElement("DIV");
-                /*make the matching letters bold:*/
-
-                // b.innerHTML = "<a href ='" + q1.concat(arr2[i]) + "'>";
-
                 b.innerHTML = "<strong>" + arr[i].name.substr(0, val.length) + "</strong>";
                 b.innerHTML += arr[i].name.substr(val.length);
                 /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i].id + "'>";
-
-                //  b.innerHTML += "</a>";
-                /*execute a function when someone clicks on the item value (DIV element):*/
-                console.log(q1.concat(arr2[i]));
-                b.addEventListener("click", function (e) {
-                    /*insert the value for the autocomplete text field:*/
-                    //inp.value = this.getElementsByTagName("input")[0].value;
-                    /*redirect to specific event with ID*/
-                    location.replace(q1.concat(this.getElementsByTagName("input")[0].value));
-                    /*close the list of autocompleted values,
-                    (or any other open lists of autocompleted values:*/
-                    closeAllLists();
-                });
-                a.appendChild(b);
+            }else{
+                b.innerHTML = arr[i].name;
             }
+            b.innerHTML += "<input type='hidden' value='" + arr[i].id + "'>";
+
+
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            console.log(q1.concat(arr2[i]));
+            b.addEventListener("click", function (e) {
+                /*insert the value for the autocomplete text field:*/
+                /*redirect to specific event with ID*/
+                location.replace(q1.concat(this.getElementsByTagName("input")[0].value));
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
         }
     });
     /*execute a function presses a key on the keyboard:*/
