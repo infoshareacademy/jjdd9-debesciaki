6d4f9ade8252eb889dc54b236ac24189959acaaf
jakubcichacki <kuba.cichacki@o2.pl>
2020-06-05 16:48:05 +0200

    Add User DAO, User View and first part of OAuth 2.0

---
+++ web/src/main/java/com/infoshareacademy/domain/view/UserView.java
@@ -0,0 +1,40 @@
+package com.infoshareacademy.domain.view;
+
+public class UserView {
+    private Long id;
+    private String name;
+    private String email;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ web/src/main/java/com/infoshareacademy/servlet/login/CallbackServlet.java
@@ -0,0 +1,54 @@
+package com.infoshareacademy.servlet.login;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeCallbackServlet;
+import com.google.api.services.oauth2.Oauth2;
+import com.google.api.services.oauth2.model.Userinfoplus;
+import com.infoshareacademy.domain.request.UserRequest;
+import com.infoshareacademy.domain.view.UserView;
+import com.infoshareacademy.mapper.GoogleUserMapper;
+import com.infoshareacademy.oauth.OAuthBuilder;
+
+import javax.ejb.EJB;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/oauth2callback")
+public class CallbackServlet extends AbstractAuthorizationCodeCallbackServlet {
+
+    @EJB
+    OAuthBuilder oAuthBuilder;
+
+    @Inject
+    GoogleUserMapper googleUserMapper;
+
+    @Override
+    protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential) throws ServletException, IOException {
+        Oauth2 oauth2 = oAuthBuilder.buildOauth(credential);
+        Userinfoplus user = oauth2.userinfo().get().execute();
+        UserRequest userRequest = googleUserMapper.mapGoogleResponseToUserRequest(user);
+        UserView userView = userService.login(userRequest);
+        new UserContextHolder(req.getSession(), userView);
+        resp.sendRedirect("/");
+    }
+
+    @Override
+    protected AuthorizationCodeFlow initializeFlow() throws ServletException, IOException {
+        return null;
+    }
+
+    @Override
+    protected String getRedirectUri(HttpServletRequest httpServletRequest) throws ServletException, IOException {
+        return null;
+    }
+
+    @Override
+    protected String getUserId(HttpServletRequest httpServletRequest) throws ServletException, IOException {
+        return null;
+    }
+}
