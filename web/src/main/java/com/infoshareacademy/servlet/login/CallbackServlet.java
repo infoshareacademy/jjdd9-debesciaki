577410e703ac1ddf32810366ad2f62ea7602ce8d
jakubcichacki <kuba.cichacki@o2.pl>
2020-06-06 10:17:15 +0200

    Prepare method when login success

--- web/src/main/java/com/infoshareacademy/servlet/login/CallbackServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/login/CallbackServlet.java
@@ -5,10 +5,12 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeCallbackServlet;
 import com.google.api.services.oauth2.Oauth2;
 import com.google.api.services.oauth2.model.Userinfoplus;
+import com.infoshareacademy.context.ContextHolder;
 import com.infoshareacademy.domain.request.UserRequest;
 import com.infoshareacademy.domain.view.UserView;
 import com.infoshareacademy.mapper.GoogleUserMapper;
 import com.infoshareacademy.oauth.OAuthBuilder;
+import com.infoshareacademy.service.UserService;
 
 import javax.ejb.EJB;
 import javax.inject.Inject;
@@ -27,13 +29,16 @@ public class CallbackServlet extends AbstractAuthorizationCodeCallbackServlet {
     @Inject
     GoogleUserMapper googleUserMapper;
 
+    @EJB
+    UserService userService;
+
     @Override
     protected void onSuccess(HttpServletRequest req, HttpServletResponse resp, Credential credential) throws ServletException, IOException {
         Oauth2 oauth2 = oAuthBuilder.buildOauth(credential);
         Userinfoplus user = oauth2.userinfo().get().execute();
         UserRequest userRequest = googleUserMapper.mapGoogleResponseToUserRequest(user);
         UserView userView = userService.login(userRequest);
-        new UserContextHolder(req.getSession(), userView);
+        new ContextHolder(req.getSession(), userView);
         resp.sendRedirect("/");
     }
 
