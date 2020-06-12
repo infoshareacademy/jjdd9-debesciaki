cd5d86c7ef7e7d71e9fe4e539aaf9f0ad8371560
Bartek <bartraw23@gmail.com>
2020-06-12 21:53:27 +0200

    Implements authorization filters redirecting to error page

---
+++ web/src/main/java/com/infoshareacademy/filter/SuperAdminAuthorizationFilter.java
@@ -0,0 +1,34 @@
+package com.infoshareacademy.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(
+        filterName = "SuperAdminAuthorizationFilter",
+        urlPatterns = {"/upload-rest", "/upload-pla", "/upload-org", "/upload-cat", "/upload-eve"}
+)
+public class SuperAdminAuthorizationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        HttpSession session = httpServletRequest.getSession(false);
+
+        boolean isAdmin = session != null && String.valueOf(session.getAttribute("role"))
+                .matches("ADMIN|SUPER_ADMIN");
+        if (!isAdmin) {
+            HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+            httpResponse.sendRedirect("https://images.squarespace-cdn.com/content/5521673ce4b09eb81bf68349/1448711636375-N0M8RIC90IEP439GGM9I/image-asset.jpeg?format=1000w&content-type=image%2Fjpeg");
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
