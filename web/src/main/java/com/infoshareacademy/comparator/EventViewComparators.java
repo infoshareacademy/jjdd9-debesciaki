e44b3902b5c653cd1719197c880de12f754cd45f
Bartek <bartraw23@gmail.com>
2020-06-15 16:13:04 +0200

    Update comparator

--- web/src/main/java/com/infoshareacademy/comparator/EventViewComparators.java
+++ web/src/main/java/com/infoshareacademy/comparator/EventViewComparators.java
@@ -3,15 +3,19 @@ package com.infoshareacademy.comparator;
 
 import com.infoshareacademy.domain.view.EventView;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 
 public class EventViewComparators {
     public static Comparator<EventView> EventDateComparatorAsc = new Comparator<EventView>() {
 
         public int compare(EventView event1, EventView event2) {
-            String eventName1 = event1.getStartDate();
-            String eventName2 = event2.getStartDate();
-            return eventName1.compareTo(eventName2);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+            LocalDateTime eventLDT1 = LocalDateTime.parse(event1.getStartDate(), formatter);
+            LocalDateTime eventLDT2 = LocalDateTime.parse(event2.getStartDate(), formatter);
+            return eventLDT1.compareTo(eventLDT2);
         }
     };
 }
