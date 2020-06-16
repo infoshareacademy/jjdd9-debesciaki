3a9e144b3df9265b4f95f0ae6d1f6116bd0c2304
Bartek <bartraw23@gmail.com>
2020-06-16 14:46:45 +0200

    Create entity for view/click statistics

---
+++ web/src/main/java/com/infoshareacademy/stat/entity/ViewStat.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.stat.entity;
+
+import com.infoshareacademy.domain.entity.Event;
+import com.infoshareacademy.domain.entity.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "view_stat")
+public class ViewStat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
