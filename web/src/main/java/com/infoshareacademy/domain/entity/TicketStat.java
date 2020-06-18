5ad588e53cbf34de8876fb1fa85de97febeb7ad3
Bartek <bartraw23@gmail.com>
2020-06-18 23:38:37 +0200

    Start entity and dao for transactions statistics

---
+++ web/src/main/java/com/infoshareacademy/domain/entity/TicketStat.java
@@ -0,0 +1,70 @@
+package com.infoshareacademy.domain.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "ticket_stat")
+public class TicketStat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "transaction_date")
+    @NotNull
+    private LocalDateTime transactionDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "ticket_type")
+    private String ticket_type;
+
+
+    public String getTicket_type() {
+        return ticket_type;
+    }
+
+    public void setTicket_type(String ticket_type) {
+        this.ticket_type = ticket_type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime viewDate) {
+        this.transactionDate = viewDate;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
