e49337a9146ade5b2943bc571d2cfcd626f0870e
Bartek <bartraw23@gmail.com>
2020-06-20 13:54:52 +0200

    Apply stat to navlink href. Clean test REST POSTs Apply frontend improvements for stat module

--- web/src/main/java/com/infoshareacademy/controller/TicketStatController.java
+++ web/src/main/java/com/infoshareacademy/controller/TicketStatController.java
@@ -1,13 +1,13 @@
 package com.infoshareacademy.controller;
 
-import com.infoshareacademy.domain.stat.TicketRatio;
 import com.infoshareacademy.service.stat.TicketStatService;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 @Path("/ticket-stat")
 @Produces(MediaType.APPLICATION_JSON)
@@ -23,35 +23,4 @@ public class TicketStatController {
                 .build();
     }
 
-    @POST
-    @Path("/eve1")
-    public Response incrementEve1(){
-        ticketStatService.updateSingleTicketStat(1L,true);
-        return Response.status(Response.Status.OK)
-                .build();
-    }
-
-    @POST
-    @Path("/eve1f")
-    public Response incrementEve1f(){
-        ticketStatService.updateSingleTicketStat(1L,false);
-        return Response.status(Response.Status.OK)
-                .build();
-    }
-
-    @POST
-    @Path("/eve2")
-    public Response incrementEve2(){
-        ticketStatService.updateSingleTicketStat(2L,true);
-        return Response.status(Response.Status.OK)
-                .build();
-    }
-
-    @POST
-    @Path("/eve2f")
-    public Response incrementEve2f(){
-        ticketStatService.updateSingleTicketStat(2L,false);
-        return Response.status(Response.Status.OK)
-                .build();
-    }
 }
